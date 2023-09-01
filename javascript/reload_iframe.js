c91408f30c715663d73edb63bfc5d9ddcbffc9f4
prolapse <117679376+PR0LAPSE@users.noreply.github.com>
2023-09-01 05:50:44 +0500

    +

--- javascript/reload_iframe.js
+++
@@ -1,7 +0,0 @@
-function reloadIframe() {
-    const iframe = document.getElementById("webui-photopea-iframe");
-    iframe.src = iframe.src;
-}
-setTimeout(() => {
-        reloadIframe();
-}, 10000);
